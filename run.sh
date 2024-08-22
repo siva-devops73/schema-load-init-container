0695586f02c684c48942f2720d87080c190f0b10
Siva Kumar <sivakumar9399959803@gmail.com>
2024-08-22 08:34:04 +0530

    schema-load-init-container

--- run.sh
+++ run.sh
@@ -15,10 +15,10 @@ git clone https://github.com/siva-devops73/${COMPONENT}
 cd  ${COMPONENT}/schema
 
 
-if [ "${SCEHMA_TYPE}" == "mongo" ]; then
+if [ "${SCHEMA_TYPE}" == "mongo" ]; then
   curl -s -L https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem -o /app/rds-combined-ca-bundle.pem
   mongo --ssl --host ${DOCDB_ENDPOINT}:27017 --sslCAFile /app/rds-combined-ca-bundle.pem --username ${DOCDB_USERNAME} --password ${DOCDB_PASSWORD} <${COMPONENT}.js
-elif [ "${SCEHMA_TYPE}" == "mysql" ]; then
+elif [ "${SCHEMA_TYPE}" == "mysql" ]; then
   echo show databases | mysql -h ${MYSQL_ENDPOINT} -u${MYSQL_USERNAME} -p${MYSQL_PASSWORD} | grep cities
   if [ $? -ne 0 ]; then
     mysql -h ${MYSQL_ENDPOINT} -u${MYSQL_USERNAME} -p${MYSQL_PASSWORD} <${COMPONENT}.sql
